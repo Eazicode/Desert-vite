db9238f6a572f9ad1998413f40756d42047dafde
Toluwalase Babatunde <technogodwr@gmail.com>
2025-02-21 17:05:33 +0100

    set up for next task

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import { useEffect } from 'react'
-import CartComponent from './Components/Cart'
-import ProductItemComponent from './Components/ProductItem'
+import { useEffect, useState } from "react";
+import CartComponent from "./Components/Cart";
+import ProductItemComponent from "./Components/ProductItem";
 // import Cart from './Components/Cart'
-import data from './desert.json'
-import './index.css'
+import data from "./desert.json";
+import "./index.css";
 
 function App() {
-  useEffect( () => {
-    console.log(data.desert)
-  }, [])
+  const [deserts, setDeserts] = useState([]);
+
+  useEffect(() => {
+    setDeserts(data.desert);
+    console.log(deserts);
+  }, [deserts]);
   return (
     <>
-    <div className="App flex justify-center pt-15">
-      <ProductItemComponent 
-      id = {1} 
-      name='Waffle'
-      Category='waffle with berries'
-      price= '$6.50'
-      image='/imgs/image-waffle-desktop.jpg'
-      />
-      <CartComponent />
-    </div>
+      <div className="App flex justify-center pt-15">
+        <ProductItemComponent
+          id={1}
+          name="Waffle"
+          Category="waffle with berries"
+          price="$6.50"
+          image="/imgs/image-waffle-desktop.jpg"
+        />
+        {
+          /* {map thru deserts here} */
+          // deserts.map()
+        }
+        <CartComponent />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
