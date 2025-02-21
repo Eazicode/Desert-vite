235ad2db4040c88d01cd799e7a9ffe5533838271
Eazicode <mistereze16@gmail.com>
2025-02-21 10:06:08 +0100

    component folder created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import CartComponent from './add-to-cart'
-import ProductItemComponent from './desert'
+import CartComponent from './Components/add-to-cart'
+import ProductItemComponent from './Components/desert'
 import './index.css'
 
 function App() {
 
   return (
     <>
-    <div className="App flex bg-rose-50 justify-between px-30 pt-15">
+    <div className="App flex justify-center pt-15">
       <ProductItemComponent />
       <CartComponent />
     </div>
