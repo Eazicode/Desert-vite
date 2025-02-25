319c9a7c39539ba4882c2046462bff8b38a0831a
Toluwalase Babatunde <technogodwr@gmail.com>
2025-02-25 19:13:08 +0100

    solved the bug that was deleting the cart item

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ function App() {
   }, [deserts]);
 
   function addToCart(obj) {
-
     // get a matching object and it's index
     // filter method returns an array of items that satisfy a given condition
     const matchedObj = CartItems.filter((item) => {
@@ -28,23 +27,26 @@ function App() {
     // if a match is found that'll be the first element in the array
     if (matchedObj[0]) {
       //gets the index of the match
-      const myIndex = CartItems.indexOf(matchedObj[0])
+      const myIndex = CartItems.indexOf(matchedObj[0]);
       //clones the match
-     const myObj = CartItems[myIndex]
+      const myObj = CartItems[myIndex];
+      console.log(myObj);
       //clones the clone but updates the amount
-     const payLoad = {...myObj,amount: myObj.amount++}
-    // remove the match from CartItems and inserts payLoad
-     const newCartItems= CartItems.splice(myIndex,1)
+      const payLoad = { ...myObj, amount: myObj.amount + 1 };
+      // remove the match from CartItems and inserts payLoad
+      const newCartItems = CartItems.filter((item) => {
+        if (item != CartItems[myIndex]) {
+          return item;
+        }
+      });
       //set the new cart item state
-      setCartItems([...newCartItems,payLoad])
-
+      setCartItems([...newCartItems, payLoad]);
     } else {
       // if  a match is not found it just
       // adds a new item to the list of cart items
       const payLoad = { ...obj, amount: 1 };
       const newCartItems = [...CartItems, payLoad];
       setCartItems(newCartItems);
-      
     }
   }
 
